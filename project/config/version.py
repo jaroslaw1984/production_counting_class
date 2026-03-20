e81846368970caa949adb4364aafa6e8947d0e8a
Jarosław Sochacki <inz.jaroslawsochacki@gmail.com>
2026-03-20 14:19:21 +0100

    Version program changed for the lastes version

--- project/config/version.py
+++ project/config/version.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 PROGRAM_NAME = "Policz produkcję"
-PROGRAM_VERSION = "2.0.2"
+PROGRAM_VERSION = "2.2.3"
 PROGRAM_YEAR = datetime.now().year
 PROGRAM_AUTHOR = "Jarosław Sochacki"
 DESCRIPTION = ("Program do szacowania czasu trwania produkcji\n"
