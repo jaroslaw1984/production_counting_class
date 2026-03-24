6ca9267bd22bb59f8e4e4fe1b8e3eafb91946a37
Jarosław Sochacki <inz.jaroslawsochacki@gmail.com>
2026-03-24 14:55:08 +0100

    refactor: relocate all aliases to a separate file

---
+++ project/config/aliases.py
@@ -0,0 +1,13 @@
+# --- aliasy ---
+ORDER_ALIASES = [
+    "zlecenie", "nr zlecenia", "zlecenie nr",
+    "auftrag", "auftragsnr", "auftragsnummer",
+    "order", "order id"
+]
+
+GRUNDPROFIL_ALIASES = [
+    "grundprofil", "grund profil", "grund-profil",
+    "podkład", "podklad", "profil podstawowy"
+]
+
+ARTICLE_ALIASES = ["artykuł", "artykul", "artikel", "article", "item", "material", "indeks"]
