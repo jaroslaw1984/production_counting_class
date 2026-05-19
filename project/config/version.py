e8e6c6c3a08160cc450d144e1c72a45e70243f95
Jarosław Sochacki <inz.jaroslawsochacki@gmail.com>
2026-05-19 11:31:19 +0200

    chore: change personal mail and program description

--- project/config/version.py
+++ project/config/version.py
@@ -1,11 +1,15 @@
 from datetime import datetime
 
-PROGRAM_NAME = "Policz produkcję"
+PROGRAM_NAME = "Program do szacowania czasu trwania produkcji oraz raportowania"
 PROGRAM_VERSION = "3.0.6"
 PROGRAM_YEAR = datetime.now().year
 PROGRAM_AUTHOR = "Jarosław Sochacki"
 DESCRIPTION = ("Program do szacowania czasu trwania produkcji\n"
             "na podstawie danych z bazy danych lub plików Excel.\n\n"
+            "Program umożliwia:\n"
+            "- Szacowanie czasu trwania produkcji na podstawie danych z bazy danych\n"
+            "- drukowanie raportów dla logistyki magazynu oraz cięcia folii\n"
+            
 )
 COMPANY_MAIL = "JSochacki@salamander-windows.com"
-PRIVATE_MAIL = "inz.jaroslaw.sochacki@gmail.com"
+PRIVATE_MAIL = "inz.jaroslawsochacki@gmail.com"
