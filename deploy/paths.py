2ff7fb8e7f2ececbf28211621881fc2d17f54e70
Jarosław Sochacki <inz.jaroslawsochacki@gmail.com>
2026-05-29 12:09:58 +0200

    create path file to the builds directory on the server

---
+++ deploy/paths.py
@@ -0,0 +1,4 @@
+
+# --- Ścieżki do katalogów i plików na serwerze --- #
+BUILD_PATH = r"\\na02\groups\3.PROJEKTY\Production Counter Program\builds"
+LATEST_JSON_PATH = r"\\na02\groups\3.PROJEKTY\Production Counter Program\latest.json"
